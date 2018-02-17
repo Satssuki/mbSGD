ac9667b0239e4e288746edff158e16581b5778c8
Chris Filippis <filippis.chris@gmail.com>
2018-02-17 20:33:38 +0200

    multiclass update

--- src/mb_sgd_new.py
+++ src/mb_sgd_new.py
@@ -73,6 +73,16 @@ def rbf_transformation(X, gamma=1):
     trainX = rbf_feature.fit_transform(X)
     return trainX
 
+# actually transform data
+def transform_data(X, tranformation_type, gamma):
+    trainX = X
+    if(tranformation_type == 'rbf'):
+        trainX = rbf_transformation(X, gamma)
+    else:
+        trainX = polynomial_transformation(X)
+
+    return trainX
+
 # merge features from labes
 def merge_labels_features(data_x, data_y):
     data = np.c_[data_x, data_y]
@@ -150,7 +160,7 @@ def shuffle_data(data_x, data_y):
     return seperate_labels_features(data)
 
 # actual the core algorthm to train our data
-def train(trainX, testX, y_train, y_test, number_of_epochs, alpha, batchSize, l2=1.0):
+def mb_sgd(trainX, y_train, number_of_epochs, alpha, batchSize, l2=1.0):
  
     # set a "random value" to init coefficients
     W = np.random.uniform(size=(trainX.shape[1],))
@@ -175,6 +185,10 @@ def train(trainX, testX, y_train, y_test, number_of_epochs, alpha, batchSize, l2
             # update coefficients
             W += -alpha * gradient
 
+    return W
+
+# use test set to get how accurate is our model
+def test(testX, y_test, W):
     predictions = list()
 
     for i in range(len(testX)):
@@ -183,8 +197,7 @@ def train(trainX, testX, y_train, y_test, number_of_epochs, alpha, batchSize, l2
         predictions.append(predicted)
 
     accuracy = accuracy_metric(y_test, predictions)
-
-    return accuracy, W
+    return accuracy
 
 # get the number of labels our dataset has
 def get_classes(labels):
@@ -192,21 +205,17 @@ def get_classes(labels):
     unique_classes = np.unique(classes)
     print 'unique_classes: ' + str(unique_classes)
 
-    if len(unique_classes) == 2:
-        return [classes]
-
     new_labels = list()
-    for i in range(len(unique_classes) - 1):
+    
+    for i in range(len(unique_classes)):
         class_tranformation = list()
         for j in range(len(classes)):
-
             if(classes[j] == unique_classes[i]):
                 class_tranformation.append(1)
             else:
                 class_tranformation.append(0)
-
-    new_labels.append(class_tranformation)
-
+        new_labels.append((unique_classes[i], np.array(class_tranformation)))
+    
     return new_labels
 
 # choosing dataset from user input
@@ -239,9 +248,10 @@ def get_parameters():
     ap.add_argument("-l", "--l2", type=float, default=1.0, help="l2")
     ap.add_argument("-g", "--gamma", type=float, default=1.0, help="gamma parameter for RBF tranformation")
     ap.add_argument("-d", "--dataset", type=str, default='diabetes', help="which dataset to use for training, options: 'wine', 'wholesales', 'diabetes', 'balance'")
+    ap.add_argument("-m", "--mode", type=str, default='cross', help="choose if either to evaluate metaparameters using crossvalidation or get accuracy, options: 'cross', 'train'")
     args = vars(ap.parse_args())
 
-    n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, data =args["epochs"], args["alpha"], args["batch_size"], args["tranformation_type"], args["folds"], args["l2"], args["gamma"], args["dataset"]
+    n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, data, mode =args["epochs"], args["alpha"], args["batch_size"], args["tranformation_type"], args["folds"], args["l2"], args["gamma"], args["dataset"], args["mode"]
 
     print 'epochs: ' + str(n_epochs)
     print 'learning rate: ' + str(l_rate)
@@ -251,13 +261,14 @@ def get_parameters():
     print 'l2: ' + str(l_2)
     print 'gamma: ' + str(gamma)
     print 'dataset: ' + str(data)
+    print 'mode: ' + mode
 
-    return n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, data
+    return n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, data, mode
 
 # cross validation method to find model
 def cross_validation(n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, X, y):
     
-    print 'data length: ' + str(len(X))
+    print 'cross validation'
 
     X_train, X_test, y_train, y_test = [], [], [], []
 
@@ -273,27 +284,90 @@ def cross_validation(n_epochs, l_rate, batch_size, tranformation_type, number_of
         trainX, minmax = minmax_fit_tranform(X_train)
         testX = minmax_tranform(X_test, minmax)
 
-        print 'Tranformation ' + tranformation_type + '...'
+        trainX = transform_data(trainX, tranformation_type, gamma)
+        testX = transform_data(testX, tranformation_type, gamma)
 
-        if(tranformation_type == 'rbf'):
-            trainX = rbf_transformation(trainX, gamma)
-            testX = rbf_transformation(testX, gamma)
-        else:
-            trainX = polynomial_transformation(trainX)
-            testX = polynomial_transformation(testX)
-            
-        print 'trainX: ' + str(len(trainX[0]))
-        print 'testX: ' + str(len(testX[0]))
+        coeffecient = mb_sgd(trainX, y_train, n_epochs, l_rate, batch_size, l2=l_2)
+        accuracy = test(testX, y_test, coeffecient)
 
-        accuracy, coeffecient = train(trainX, testX, y_train, y_test, n_epochs, l_rate, batch_size, l2=l_2)
         accuracies.append(accuracy)
         coeffecients.append(coeffecient)
 
-    print coeffecients
     print 'accuracies: ' + str(accuracies)
     print 'avg accuracy: ' + str(np.mean(accuracies))
 
-n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, data = get_parameters()
+# cross validation method to find model
+def multiclass_cross_validation(n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, X, ovr_y, y):
+    
+    print 'multiclass cross validation'
+
+    class_coeffecients = list()
+    for class_y in ovr_y:
+        X_train, X_test, y_train, y_test = [], [], [], []
+
+        kf = KFold(n_splits=number_of_folds)
+
+        accuracies = list()
+        coeffecients = list()
+
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = class_y[1][train_index], class_y[1][test_index]
+
+            trainX, minmax = minmax_fit_tranform(X_train)
+            testX = minmax_tranform(X_test, minmax)
+
+            trainX = transform_data(trainX, tranformation_type, gamma)
+            testX = transform_data(testX, tranformation_type, gamma)
+
+            coeffecient = mb_sgd(trainX, y_train, n_epochs, l_rate, batch_size, l2=l_2)
+            accuracy = test(testX, y_test, coeffecient)
+
+            accuracies.append(accuracy)
+            coeffecients.append(coeffecient)
+        
+        class_coeffecients.append((class_y[0], coeffecients[0]))
+
+        print 'class ' + str(class_y[0]) + ' accuracies: ' + str(accuracies)
+        print 'class ' + str(class_y[0]) + ' avg accuracy: ' + str(np.mean(accuracies))
+
+    x_in, minmax = minmax_fit_tranform(X)
+    x_in = transform_data(x_in, tranformation_type, gamma)
+
+    predictions = list()
+
+    for ii in range(len(y)):
+        label_result_index = 0
+        label_result = 0
+        
+        for class_label in class_coeffecients:
+            predicted = predict(x_in[ii], class_label[1])
+
+            if label_result < predicted:
+                label_result = predicted
+                label_result_index = class_label[0]
+            
+        predictions.append(label_result_index)
+            
+    accuracy = accuracy_metric(y, predictions)
+
+    print accuracy
+
+# train  
+def train(n_epochs, l_rate, batch_size, tranformation_type, l_2, gamma, X, y):
+    trainX, minmax = minmax_fit_tranform(X)
+    trainX = transform_data(trainX, tranformation_type, gamma)
+    return mb_sgd(trainX, y, n_epochs, l_rate, batch_size, l2=l_2)
+
+# multiclass train  
+def multiclass_train(n_epochs, l_rate, batch_size, tranformation_type, l_2, gamma, X, ovr_y):
+    class_coeffecients = list()
+    for class_y in ovr_y:
+        coef = train(n_epochs, l_rate, batch_size, tranformation_type, l_2, gamma, X, class_y[1])
+        class_coeffecients.append((class_y[0], coef))
+    return class_coeffecients
+
+n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, data, mode = get_parameters()
 n_epochs = 2000
 l_rate = 0.01 
 batch_size = 50
@@ -302,9 +376,50 @@ number_of_folds = 5
 l_2 = 1.0
 gamma = 1.0
 data = 'diabetes'
+mode = 'cross'
+leave_out = 0.05
+
 # load data
 dataset = load_data(data)
 X, y = seperate_labels_features(dataset)
 # X, y = make_blobs(n_samples=700, n_features=2, centers=2, cluster_std=2.5, random_state=95)
 
-cross_validation(n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, X, y)
+index_to_leave_out = int(round(len(X) * leave_out))
+
+print 'data length: ' + str(len(X))
+print 'leaving out : ' + str(index_to_leave_out) + \
+    ' records (' + str(leave_out * 100) + '%)'
+
+# leaving some data out of model
+X_out = X[-index_to_leave_out:]
+y_out = y[-index_to_leave_out:]
+
+X_in = X[:-index_to_leave_out]
+y_in = y[:-index_to_leave_out]
+
+
+classes = get_classes(y_in)
+
+if mode == 'cross':
+    if len(classes) == 2:
+        cross_validation(n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, X_in, y_in)
+    else:
+        multiclass_cross_validation(n_epochs, l_rate, batch_size, tranformation_type, number_of_folds, l_2, gamma, X_in, classes, y_in)
+else:
+
+    print 'evaluate'
+    out_X, minmax = minmax_fit_tranform(X_out)
+
+    if len(classes) == 2:
+        coeffecients = train(n_epochs, l_rate, batch_size, tranformation_type, l_2, gamma, out_X, y_out)
+        predictions = list()
+
+        for i in range(len(out_X)):
+            predicted = predict(out_X[i], coeffecients)
+            predicted = round(predicted)
+            predictions.append(predicted)
+
+        accuracy = accuracy_metric(y_out, predictions)
+        print accuracy
+    else:
+        coeffecients =multiclass_train(n_epochs, l_rate, batch_size, tranformation_type,  l_2, gamma, out_X, classes)
